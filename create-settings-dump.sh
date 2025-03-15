19bf7df401f96cc0b1b6ea22233e9bc2bb67a5ea
Issa Rice <riceissa@gmail.com>
2025-03-14 21:11:59 -0700

    add this too

--- create-settings-dump.sh
+++ create-settings-dump.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-/usr/bin/vim -u NONE -c 'source dump_settings.vim|write! out/vim-none.txt|quit'
-/usr/bin/vim -Nu NORC -c 'source dump_settings.vim|write! out/vim-norc.txt|quit'
-/usr/bin/vim --clean -c 'source dump_settings.vim|write! out/vim-clean.txt|quit'
-nvim --clean -c 'source dump_settings.vim|write! out/nvim.txt|quit'
+mkdir -p out
+
+# /usr/bin/vim -u NONE -c 'source ~/sensible.vim' -c 'source dump_settings.vim|write! out/vim-none.txt|quit'
+# /usr/bin/vim -Nu NORC -c 'source ~/sensible.vim' -c 'source dump_settings.vim|write! out/vim-norc.txt|quit'
+# /usr/bin/vim --clean -c 'source ~/sensible.vim' -c 'source uniform.vim' -c 'source dump_settings.vim|write! out/vim-clean-sensible.txt|quit'
+
+
+/usr/bin/vim -Nu NONE -c 'source uniform.vim' -c 'source ~/.vim/plugged/vim-commentary/plugin/commentary.vim' -c 'source ~/.vim/plugged/vim-visual-star-search/plugin/visual-star-search.vim' -c 'source dump_settings.vim|write!|quit' out/vim-nu-none.txt
+nvim --clean -c 'source uniform.vim' -c 'source ~/.vim/plugged/vim-commentary/plugin/commentary.vim' -c 'source ~/.vim/plugged/vim-visual-star-search/plugin/visual-star-search.vim' -c 'source dump_settings.vim|write!|quit' out/nvim.txt
